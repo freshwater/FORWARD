876251cf4387ac58ba80135819c4e0c719515db1
amr <amr@Tonys-MacBook-Pro.local>
2020-10-15 22:37:36 -0700

    Demonstration clean up

--- interface/retro_module.py
+++ interface/retro_module.py
@@ -50,7 +50,7 @@ class RetroModule2(Module):
         self.states = retro.data.list_states(self.game)
         self.state = self.states[0]
         self.environment = None
-        self.commitment_interval = 6
+        self.commitment_interval = 16
         self.encodings = False
         self.reset()
 
@@ -58,6 +58,11 @@ class RetroModule2(Module):
         if self.environment != None:
             self.environment.close()
 
+        self.frame_previous = None
+        self.index_previous = -1
+        self.frame_history = []
+        self.frame_deltas_history = []
+        self.last_interface = None
         self.environment = retro_server.RetroClient(game=self.game, state=self.state)
 
     def game_select(self, game):
@@ -76,9 +81,19 @@ class RetroModule2(Module):
     def encoding_display_set(self, key, value):
         self.encodings = value
 
+    def step(self, action, commitment_interval):
+        if (self.game == 'SuperMarioBros-Nes'
+            and self.index_previous < 120
+            and action == RetroModule2.actions_map['RIGHT']):
+            commitment_interval = 22
+            action = [a + b for a, b in zip(RetroModule2.actions_map['B'], RetroModule2.actions_map['RIGHT'])]
+            # ☝️😂👌👌
+
+        self.last_interface = self.environment.step(action, commitment_interval)
+
     def controls(self):
-        take_action_f = lambda action: lambda: self.environment.step(RetroModule2.actions_map[action],
-                                                                     commitment_interval=self.commitment_interval)
+        take_action_f = lambda action: lambda: self.step(RetroModule2.actions_map[action],
+                                                         commitment_interval=self.commitment_interval)
 
         return [Button(label=action, on_click=take_action_f(action))
                 for action in RetroModule2.actions_map.keys()]
@@ -95,30 +110,93 @@ class RetroModule2(Module):
         })
 
     def interface(self):
-        (self.frame, data_elements) = self.environment.interface_render2(self.encodings)
+        # Both Retro and PyTorch have issues with multiple instances on a single process,
+        # necessitating a multiprocess mechanism.
+        # Generally we try to keep as much data on this side of the serialization.
+        # Controls/events in particular are easier to use on this side of the boundary
+        # since they can't directly alter state from the other side.
+
+        if self.last_interface is not None:
+            objects = self.last_interface
+        else:
+            objects = self.environment.interface_render2(self.encodings)
+
+        self.frame = objects["Frame"]
+
+        if objects["FrameIndex"] != self.index_previous:
+            frame = self.frame
+
+            # add alpha channel
+            padded = 255 * np.ones(np.array(frame.shape) + [0, 0, 1])
+            padded[:, :, :frame.shape[2]] = frame
+            frame = padded
+
+            # make background transparent
+            colors, counts = np.unique(frame.reshape(-1, 4), axis=0, return_counts=True)
+            background = colors[counts.argmax()]
+            mask = (1 + frame.reshape(-1, 4)).dot(1 + background) == (1 + background).dot(1 + background)
+            frame.reshape(-1, 4)[mask] = [0, 0, 0, 0]
+
+            self.frame_history.append(frame[::2, ::2])
+
+        if self.frame_previous is not None:
+            mask = ((1 + frame.reshape(-1, 4)).dot(np.array([1, 2, 4, 8])) ==
+                    (1 + self.frame_previous.reshape(-1, 4)).dot(np.array([1, 2, 4, 8])))
+
+            frame_x = frame.copy()
+            frame_x.reshape(-1, 4)[mask] = [0, 0, 0, 0]
+            frame_x.reshape(-1, 4)[~mask] = [0.5, 0.5, 0.5, 1]
+
+            if objects["FrameIndex"] != self.index_previous:
+                self.frame_deltas_history.append(frame_x[::3, ::3])
+
+        self.index_previous = objects["FrameIndex"]
+        self.frame_previous = frame
+        empty = np.ones(frame[::3, ::3].shape)
+        empty[::,::,3] = 0
 
         game_choices = SelectionList(options=RetroModule2.games_list,
                                      selected_option=self.game,
                                      on_change=self.game_select)
 
-        state_choices = SelectionList(options=self.states,
-                                      selected_option=self.state,
-                                      on_change=self.state_select,
-                                      is_enabled=len(self.states) > 1)
+        game_state_choices = SelectionList(options=self.states,
+                                           selected_option=self.state,
+                                           on_change=self.state_select,
+                                           is_enabled=len(self.states) > 1)
 
         encodings_choices = CheckList(options=["Encodings"],
                                       selected_options=["Encodings"] if self.encodings else [],
                                       on_check=self.encoding_display_set)
 
-        return ([self.settings()] + [game_choices, state_choices]
-                + data_elements[:1]
-                + [self.controls()]
-                + [encodings_choices]
-                + data_elements[1:]
-                + [{"Frame Skip": NumberInput(minimum=1, maximum=None,
-                                                       value=self.commitment_interval,
-                                                       on_change=self.commitment_interval_set)}]
-                + [Button(label="RESET", on_click=self.reset)])
+        return [
+            self.settings(),
+            game_choices,
+            game_state_choices,
+            Image(self.frame, elements=objects["Elements"], display_scale=2),
+            self.controls(),
+
+            {"Encodings": Image(objects["EncodingsArray"], display_scale=12),
+             "PCA RGB": objects["PCA RGB"]},
+
+            {"R": Image(self.frame[::2,::2,0], color_map='Grayscale'),
+             "G": Image(self.frame[::2,::2,1], color_map='Grayscale'),
+             "B": Image(self.frame[::2,::2,2], color_map='Grayscale')},
+
+            ArrayPlot3D(self.frame_history, _sequence_hacks_much_spooky_do_not_use="secr3tcode"),
+            objects["Actions"],
+            FileImage(objects["DiffsImageFile"]),
+            FileImage(objects["PCAEncodingsImageFile"]),
+            ArrayPlot3D(self.frame_deltas_history or [empty], _sequence_hacks_much_spooky_do_not_use="secr3tcode"),
+
+            {"Frame Index": objects["FrameIndex"],
+             "Blocks Count": len(objects["Blocks"])},
+            {"Blocks": objects["Blocks"]},
+
+            [{"Frame Skip": NumberInput(minimum=1, maximum=None,
+                                        value=self.commitment_interval,
+                                        on_change=self.commitment_interval_set)}],
+            Button(label="RESET", on_click=self.reset)
+        ]
 
 
 class RetroModule1(Module):
--- interface/retro_server.py
+++ interface/retro_server.py
@@ -4,17 +4,10 @@ import torch.nn.functional as F
 import uuid
 import numpy as np
 import retro
-import os
 
 import matplotlib.pyplot as plt
 
-import forward as fwd
-
-def block_partition(matrix, block_width):
-    matrix = matrix.reshape(-1, block_width, matrix.shape[0] // block_width, block_width, 3)
-    matrix = matrix.transpose(2, 1).reshape(-1, block_width, block_width, 3)
-
-    return matrix
+import forward
 
 class Environment3:
     import pickle
@@ -25,41 +18,35 @@ class Environment3:
     def __init__(self, game, state, actions=[], bk2_location=None):
         self.environment = retro.make(game=game, state=state, record=bk2_location)
 
-        self.blocks_seen = []
-        self.blocks_seen_urls = []
         self.blocks_seen_images = []
 
         self.encodings = set()
         self.encodings_frame = set()
         self.encodings_all = []
 
-        random_key = str(uuid.uuid4())
-        self.image_files_folder = random_key
-        os.makedirs('/tmp/' + self.image_files_folder)
-
         if bk2_location:
+            random_key = str(uuid.uuid4())
+
             # Not sure how retro's recording mechanism works, but this seems
             # to force a bk2 file to be written in the bk2_location.
             self.environment.unwrapped.record_movie(f'/tmp/unused-{random_key}._bk2')
 
-        self.frame = self.environment.reset()
-        self.blocks_identify(self.frame)
-        self.frame_index = 0
-        self.frame_history = []
-        self.frame_previous = None
-        self.frame_deltas_history = []
-
-        self.index_previous = -1
-
-        self.actions_all = []
-        self.commitment_intervals_all = []
-
         asymmetric = torch.linspace(0.5, 1.5, 16*16*3)**3
         asymmetric = asymmetric.numpy()
-        
+
         aa = torch.tensor(asymmetric).reshape(16, 16, 3).unsqueeze(0).permute(0, 3, 1, 2)
         filter_ = torch.tensor(aa, dtype=torch.float)
         self.filter_ = filter_ / 16 / 16 / 3 / 255
+
+        self.encodings_frame_array = []
+
+        self.frame = self.environment.reset()
+        self.blocks_identify(self.frame)
+        self.frame_index = 0
+        self.frame_deltas_history = []
+
+        self.actions_all = []
+        self.commitment_intervals_all = []
 
         if actions:
             # note this produces different encodings, since the default
@@ -84,71 +71,54 @@ class Environment3:
         self.blocks_identify(self.frame)
         self.encodings_all.append(self.encodings_frame)
 
-        return self.frame # , reward, is_done, information
+        return self.interface_render2(False)
 
     def close(self):
         self.environment.unwrapped.stop_record()
         if env := self.environment:
             env.render(close=True)
             env.close()
-            # del self.environment
-
-    # __del__ = close
 
     def blocks_identify(self, frame):
-        obs = torch.tensor(frame)
+        frame_chw = torch.tensor(frame, dtype=torch.float).permute(2, 0, 1)
+        frame_chw = frame_chw.unsqueeze(0)
 
-        obs = torch.cat([obs,
-                         torch.zeros(16, 240, 3)]).long()
+        unfolded = F.unfold(input=frame_chw,
+                            kernel_size=(16, 16),
+                            stride=(16, 16))
 
-        blocks = block_partition(obs, 16)
+        blocks_chw = unfolded.squeeze().T.reshape(-1, 3, 16, 16)
+        blocks_hwc = blocks_chw.permute(0, 2, 3, 1)
 
-        blocks = blocks[4*15:-2*15]
+        # Encoding the full frame can acount for the "entropy" in the score
+        # for better or worse, but here we are only encoding the middle region
+        # to not spam the interface.
+        blocks_hwc = blocks_hwc[4*15:-1*15]
 
-        exponent = 2
+        encodings_x = F.conv2d(input=frame_chw,
+                               weight=self.filter_,
+                               stride=16)
 
-        asymmetric = torch.linspace(0.5, 1.5, 16*16*3)**3
-        # encodings = [((block.float() / 255 / 16 / 16).sum()**exponent).item() for block in blocks]
-        # encodings = [hash(((block.float() / 255 / 16 / 16).sum()**exponent).item()) % 255 for block in blocks]
-        # encodings = [((block.flatten().float() @ asymmetric / 16 / 16 / 3 / 255) - 0.5).item() for block in blocks]
-        encodings_frame = [((block.flatten().float() @ asymmetric / 16 / 16 / 3 / 255)).item() for block in blocks]
+        encodings_x = encodings_x[:, :, 4:-1].squeeze()
+        self.encodings_frame_array = encodings_x
+        encodings_frame = encodings_x.flatten().numpy()
 
-        diffs = set(encodings_frame).difference(self.encodings)
+        new_encodings = set(encodings_frame).difference(self.encodings)
 
-        written = set()
-        for encoding, block in zip(encodings_frame, blocks):
-            if encoding in diffs and encoding not in written:
-                written.add(encoding)
+        # A single frame can contain multiple instances of a new block.
+        # Uniqueness can be determined by the encoding.
+        already_added = set()
+        for encoding, block in zip(encodings_frame, blocks_hwc):
+            if encoding in new_encodings and encoding not in already_added:
+                already_added.add(encoding)
 
-                plt.imsave(f"/tmp/{self.image_files_folder}/{str(encoding)[2:]}.png", block.byte().numpy())
-                self.blocks_seen_urls.append(f"{self.image_files_folder}/{str(encoding)[2:]}.png")
-
-                image = fwd.Image(array=block.byte(), display_scale=0.5)
-                image.json() # ??
+                image = forward.Image(array=block.byte(), display_scale=0.5)
+                image.json() # cache base64 image before it is serialized for multiprocess
 
                 self.blocks_seen_images.append((encoding, image))
 
-
-        new_blocks = [block for encoding, block in zip(encodings_frame, blocks)
-                            if encoding in diffs]
-
-        new_blocks = set(tuple(block.flatten().tolist()) for block in new_blocks)
-        new_blocks = [torch.tensor(block).reshape(16, 16, 3) for block in new_blocks]
-
-        # update
-        new_blocks = [block.tolist() for block in new_blocks]
-        self.blocks_seen.extend(new_blocks)
-
-        # if random.random() < 0.1:
-        #     self.blocks_seen = sorted(self.blocks_seen, key=lambda x: tuple(torch.tensor(x).flatten().tolist()))
-
-        self.encodings.update(diffs)
+        self.encodings.update(new_encodings)
         self.encodings_frame = encodings_frame
-
-
-    def interface_render(self):
-        self.blocks_seen_urls = sorted(self.blocks_seen_urls)
-        return self.frame.tolist(), list(self.encodings_frame), self.blocks_seen_urls, self.frame_index
 
     def block_pca(self):
         import forward
@@ -180,39 +150,31 @@ class Environment3:
         return forward.Image(rescaled, display_scale=12, inspect_form=ts.reshape(-1, 15, 3))
 
     def interface_render2(self, show_encodings):
-        tensor = torch.tensor(self.frame, dtype=torch.float).unsqueeze(0)
-        images = tensor.permute(0, 3, 1, 2)
-
-        output = F.conv2d(input=images,
-                          weight=self.filter_,
-                          stride=16)
-
-        output = output[:, :, 4:-1].squeeze()
-        output_array = output
-
-        output = [[str(e*10)[:4] for e in row]
-                  for row in output.tolist()]
+        output_array = self.encodings_frame_array.numpy()
 
         height, width, depth = self.frame.shape
 
-        import hashlib
-        to_number = lambda x: (int(hashlib.sha1(x.encode()).hexdigest(), 16) % 100_000_000) / 100_000_000
-
         regions = []
         if show_encodings:
+            import hashlib
+            to_number = lambda x: (int(hashlib.sha1(x.encode()).hexdigest(), 16) % 100_000_000) / 100_000_000
+
+            output = [[str(e*10)[:4] for e in row]
+                      for row in output_array.tolist()]
+
             seed = "6"
             for i in range(output_array.shape[0]):
                 for j in range(output_array.shape[1]):
                     value = output_array[i][j].item()
                     r, g, b = (to_number(str(value) + seed + color) for color in ["Red", "Green", "Blue"])
-                    regions.append(fwd.Region(geometry=[[j*16, (i+4)*16], [j*16 + 16, (i+4)*16 + 16]],
-                                            color=[r, g, b, 0.2],
-                                            label=output[i][j],
-                                            label_color=[1, 1, 0, 0.75]))
+                    regions.append(forward.Region(geometry=[[j*16, (i+4)*16], [j*16 + 16, (i+4)*16 + 16]],
+                                                  color=[r, g, b, 0.2],
+                                                  label=output[i][j],
+                                                  label_color=[1, 1, 0, 0.75]))
 
-        frame_index_region = fwd.Region(geometry=[[width - 16, 0], [width, 8]],
-                                        color=[0, 0, 0, 0],
-                                        label=self.frame_index)
+        frame_index_region = forward.Region(geometry=[[width - 16, 0], [width, 8]],
+                                            color=[0, 0, 0, 0],
+                                            label=self.frame_index)
 
         regions.append(frame_index_region)
 
@@ -248,71 +210,17 @@ class Environment3:
         filename2 = f'/tmp/{uuid.uuid4()}.png'
         plt.savefig(filename2, dpi=300)
 
-        def ring(width, radius):
-            xy = np.indices([width, width])
-            arr = np.linalg.norm(xy.T - width // 2, axis=2).T
-            arr = np.abs(arr - radius)
-            arr = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
-            arr = (1 - arr)**10
-            arr[arr < 0.85] = arr[arr < 0.85]**4
-
-            return arr
-
-        frame = self.frame
-        padded = 255*np.ones((frame.shape[0], frame.shape[1], frame.shape[2]+1))
-        padded[:, :, :frame.shape[2]] = frame
-        frame = padded
-
-        colors, counts = np.unique(frame.reshape(-1, 4), axis=0, return_counts=True)
-        background = colors[counts.argmax()]
-        mask = (1 + frame.reshape(-1, 4)).dot(1 + background) == (1 + background).dot(1 + background)
-        frame.reshape(-1, 4)[mask] = [0, 0, 0, 0]
-
-        if self.frame_index != self.index_previous:
-            self.frame_history.append(frame[::2, ::2])
-
-        if self.frame_previous is not None:
-
-            mask = ((1 + frame.reshape(-1, 4)).dot(np.array([1, 2, 4, 8])) ==
-                    (1 + self.frame_previous.reshape(-1, 4)).dot(np.array([1, 2, 4, 8])))
-
-            frame_x = frame.copy()
-            frame_x.reshape(-1, 4)[mask] = [0, 0, 0, 0]
-            frame_x.reshape(-1, 4)[~mask] = [0.5, 0.5, 0.5, 1]
-
-            if self.frame_index != self.index_previous:
-                self.frame_deltas_history.append(frame_x[::3, ::3])
-
-        if self.frame_index != self.index_previous:
-            self.index_previous = self.frame_index
-
-        self.frame_previous = frame
-        empty = np.ones(frame[::3, ::3].shape)
-        empty[::,::,3] = 0
-
-        return (self.frame, [fwd.Image(self.frame, elements=regions, display_scale=2),
-
-                {"Encodings": fwd.Image(output_array, display_scale=12),
-                 "PCA RGB": self.block_pca()},
-
-                {"R": fwd.Image(self.frame[::2,::2,0], color_map='Grayscale'),
-                 "G": fwd.Image(self.frame[::2,::2,1], color_map='Grayscale'),
-                 "B": fwd.Image(self.frame[::2,::2,2], color_map='Grayscale')},
-
-                fwd.ArrayPlot3D(self.frame_history, _sequence_hacks_much_spooky_do_not_use="secr3tcode"),
-
-                fwd.Image(self.actions_all),
-                fwd.FileImage(file=filename1),
-                fwd.FileImage(file=filename2),
-
-                fwd.ArrayPlot3D(self.frame_deltas_history or [empty], _sequence_hacks_much_spooky_do_not_use="secr3tcode"),
-
-                fwd.Array(output),
-
-                {'Frame Index': self.frame_index,
-                 'Blocks Count': len(block_images)},
-                {'Blocks': block_images}
-        ])
+        return {
+            "Frame": self.frame,
+            'FrameIndex': self.frame_index,
+            "Elements": regions,
+            "DiffsImageFile": filename1,
+            "PCAEncodingsImageFile": filename2,
+            "EncodingsArray": output_array,
+            "PCA RGB": self.block_pca(),
+            "Actions": forward.Image(self.actions_all),
+            'Blocks': block_images
+        }
 
 
 class RetroClient:
@@ -353,24 +261,12 @@ class RetroClient:
                                               'commitment_interval': commitment_interval}])
         return self.parent_connection.recv()
 
-    def interface_render(self):
-        self.parent_connection.send(['interface_render', {}])
-        return self.parent_connection.recv()
-
-    def frame(self):
-        self.parent_connection.send(['[get_attribute]', {'attribute': 'frame'}])
-        return self.parent_connection.recv()
-
     def interface_render2(self, show_encodings):
         self.parent_connection.send(['interface_render2', {'show_encodings': show_encodings}])
         return self.parent_connection.recv()
 
     def close(self):
         self.parent_connection.send(['close', {}])
-        return self.parent_connection.recv()
-
-    def image_files_folder(self):
-        self.parent_connection.send(['[get_attribute]', {'attribute': 'image_files_folder'}])
         return self.parent_connection.recv()
 
     def actions_commitment_intervals(self):
